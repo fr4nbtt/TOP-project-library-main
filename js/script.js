0f6ce48625ae8d17f39da60ba3850c08eb954a3b
fr4nbtt <batet.francisco@gmail.com>
2024-12-17 16:39:07 -0300

    feat: add Book constructor and addBookToLibrary functions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-"use strict"
+"use strict";
+
+const myLibrary = [];
+
+/* Book Constructor function */
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+}
+
+/* Test */
+addBookToLibrary("The Dark", "Mark Boer", 321, true);
+addBookToLibrary("VSC History", "Radu Vida", 141, false);
+console.table(myLibrary);
