f8561b085668527ec5c635964d1a4b8065d58950
fr4nbtt <batet.francisco@gmail.com>
2024-12-17 18:15:32 -0300

    feat: add modern-normalize.css, basic card styling, displayBooks function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 "use strict";
 
+/* Selectors */
+const libraryContainer = document.querySelector("#library-container");
+
+/* Library array of book objects */
 const myLibrary = [];
 
 /* Book Constructor function */
@@ -10,12 +14,37 @@ function Book(title, author, pages, read) {
   this.read = read;
 }
 
+/*  */
 function addBookToLibrary(title, author, pages, read) {
   const newBook = new Book(title, author, pages, read);
   myLibrary.push(newBook);
+  displayBooks();
 }
 
-/* Test */
+/*  */
+function displayBooks() {
+  libraryContainer.innerHTML = "";
+
+  myLibrary.forEach((book) => {
+    // Create a new element for each book
+    const bookCard = document.createElement("div");
+    bookCard.classList.add("book-card");
+
+    // Add book details (title, author, etc.)
+    bookCard.innerHTML = `
+        <h3>${book.title}</h3>
+        <p>Author: ${book.author}</p>
+        <p>Pages: ${book.pages}</p>
+        <p>Read: ${book.read ? "Yes" : "No"}</p>
+      `;
+
+    // Append to the library container
+    libraryContainer.appendChild(bookCard);
+  });
+}
+
+/* Tests */
 addBookToLibrary("The Dark", "Mark Boer", 321, true);
 addBookToLibrary("VSC History", "Radu Vida", 141, false);
+addBookToLibrary("Old World Jazz", "Andrea Redthorn", 243, true);
 console.table(myLibrary);
